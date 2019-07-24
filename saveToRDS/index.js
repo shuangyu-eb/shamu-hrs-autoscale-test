dcd0e7b78c4a16d4d8f6f5e1c67b7f4e220535f8
qi-eb <jiaqi@easternbay.cn>
2019-07-24 11:44:32 +0800

    195: add created time

--- saveToRDS/index.js
+++ saveToRDS/index.js
@@ -45,7 +45,7 @@ exports.handler = async (event, context) => {
     folder_id = parseInt(folder_id) || null;
     signature_template_id = parseInt(signature_template_id) || null;
 
-    const sql = `INSERT INTO documents(title, url, user_id, company_id ,folder_id, signature_template_id, created_at) VALUES ('${title}','${key}',${user_id},${company_id},${folder_id},${signature_template_id},${new Date()});`;
+    const sql = `INSERT INTO documents(title, url, user_id, company_id ,folder_id, signature_template_id, created_at) VALUES ('${title}','${key}',${user_id},${company_id},${folder_id},${signature_template_id},${new Date().toISOString()});`;
 
     client.query(sql, (err, res) => {
       if (err) {
