9680fdbc59b25bbef6edc00250417e658da5766d
zhao jian <zhaojian@easternbay.cn>
2019-09-03 16:59:31 +0800

    466: sync with design

--- auth0manager/config/config.py
+++
@@ -1,4 +0,0 @@
-auth0Domain = 'shamu-develop.auth0.com'
-auth0ClientId = 'm4NqIEn5xHATGMFbzZrXFM5F773NF32s'
-auth0ClientSecret = 'PvPm4YDdjLMcbVZ2zdyPEyO-Bx1q_-cgONbfgXKqbazpPnFMS7_2fNH7-pW4TWXl'
-auth0ApiIdentifier = 'hrs-backend'
