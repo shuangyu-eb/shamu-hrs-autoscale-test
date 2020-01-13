9c06115d0dde04a8c9cb0bf1144082f03c30cbfe
Max Zhao <40556623+max-eb@users.noreply.github.com>
2020-01-13 17:42:54 +0800

    1443: Remove auth0 manager
    1443: Remove auth0 manager

--- auth0manager/role.py
+++
@@ -1,6 +0,0 @@
-class Role:
-    def __init__(self, role_name, role_description, permission_type, permissions):
-        self.role_name = role_name
-        self.role_description = role_description
-        self.permission_type = permission_type
-        self.permissions = permissions
