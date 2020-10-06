e5b9e6f0776bcd608973d9be7c2510c1e8393786
mshumaker-indeed <50824398+mshumaker-indeed@users.noreply.github.com>
2020-10-06 11:31:32 -0600

    added more errors (#270)

--- timezone-migration-app/src/main/java/helpers/GoogleMapsHelper.java
+++ timezone-migration-app/src/main/java/helpers/GoogleMapsHelper.java
@@ -1,4 +1,6 @@
+
 package helpers;
+
 import com.google.maps.GeoApiContext;
 import com.google.maps.GeocodingApi;
 import com.google.maps.TimeZoneApi;
@@ -18,13 +20,13 @@ public class GoogleMapsHelper {
         this.googleGeoCodingApiKey = googleGeoCodingApiKey;
     }
 
-    public String findTimezone(final TimezoneAddress timezoneAddress) throws Exception {
+    public String findTimezone(final TimezoneAddress timezoneAddress) {
         String zipCodeTimezone = findTimezoneByZipCode(timezoneAddress.getPostalCode());
         if (zipCodeTimezone!=null){
             return zipCodeTimezone;
         }
         String cityStateTimezone = findTimezoneByCityState(timezoneAddress.getCity(),
-        timezoneAddress.getStateProvinceCode());
+                timezoneAddress.getStateProvinceCode());
         if (cityStateTimezone != null){
             return cityStateTimezone;
         }
@@ -42,78 +44,88 @@ public class GoogleMapsHelper {
                 return null;
             }
         } catch (Exception e) {
-            log.warn("Error in Zipcode Timezone, going to city/state");
+            log.warn("Error in Zipcode Timezone, going to city/state " + postalCode,e);
             return null;
         }
     }
 
-    private String findTimezoneByCityState(final String city, final String state) throws Exception {
-        final Optional<LatLng> location = findLocationByCityState(city, state);
-        if (location.isPresent()) {
-            final LatLng latLng = location.get();
-              return findTimezoneByLocation(latLng);
-        }else{
+    private String findTimezoneByCityState(final String city, final String state){
+        try {
+            final Optional<LatLng> location = findLocationByCityState(city, state);
+            if (location.isPresent()) {
+                final LatLng latLng = location.get();
+                return findTimezoneByLocation(latLng);
+            } else {
+                return null;
+            }
+        } catch (Exception e) {
+            log.warn("Error in City/State Timezone, going to state " + city + ", " + state,e);
             return null;
-        } 
+        }
     }
 
-    private String findTimezoneByState(final String state) throws Exception {
-        final Optional<LatLng> location = findLocationByState(state);
-        if (location.isPresent()) {
-            final LatLng latLng = location.get();
-              return findTimezoneByLocation(latLng);
-        }else{
+    private String findTimezoneByState(final String state) {
+        try {
+            final Optional<LatLng> location = findLocationByState(state);
+            if (location.isPresent()) {
+                final LatLng latLng = location.get();
+                return findTimezoneByLocation(latLng);
+            } else {
+                return null;
+            }
+        } catch (Exception e) {
+            log.warn("Error in State " +  state,e);
             return null;
-        } 
+        }
     }
 
     private Optional<LatLng> findLocationByState(final String state)
-      throws Exception {
-    final GeoApiContext geoCodingApiContext =
-        new GeoApiContext.Builder().apiKey(googleGeoCodingApiKey).build();
-    final GeocodingResult[] results;
-    results =
-        GeocodingApi.geocode(geoCodingApiContext, "")
-            .components(new ComponentFilter("administrative_area", state))
-            .await();
-    geoCodingApiContext.shutdown();
-    return results.length == 0 ? Optional.empty() : Optional.of(results[0].geometry.location);
+            throws Exception {
+        final GeoApiContext geoCodingApiContext =
+                new GeoApiContext.Builder().apiKey(googleGeoCodingApiKey).build();
+        final GeocodingResult[] results;
+        results =
+                GeocodingApi.geocode(geoCodingApiContext, "")
+                        .components(new ComponentFilter("administrative_area", state))
+                        .await();
+        geoCodingApiContext.shutdown();
+        return results.length == 0 ? Optional.empty() : Optional.of(results[0].geometry.location);
     }
 
     private Optional<LatLng> findLocationByCityState(final String city, final String state)
-      throws Exception {
-          final GeoApiContext geoCodingApiContext = 
-          new GeoApiContext.Builder().apiKey(googleGeoCodingApiKey).build();
-          final GeocodingResult[] results;
-          results =
-            GeocodingApi.geocode(geoCodingApiContext, "")
-                .components(new ComponentFilter("administrative_area", state),
-                new ComponentFilter("locality",city))
-                .await();
+            throws Exception {
+        final GeoApiContext geoCodingApiContext =
+                new GeoApiContext.Builder().apiKey(googleGeoCodingApiKey).build();
+        final GeocodingResult[] results;
+        results =
+                GeocodingApi.geocode(geoCodingApiContext, "")
+                        .components(new ComponentFilter("administrative_area", state),
+                                new ComponentFilter("locality",city))
+                        .await();
         geoCodingApiContext.shutdown();
         return results.length == 0 ? Optional.empty() : Optional.of(results[0].geometry.location);
     }
 
     private Optional<LatLng> findLocationByPostalCode(final String postalCode)
-      throws Exception {
-    final GeoApiContext geoCodingApiContext =
-        new GeoApiContext.Builder().apiKey(googleGeoCodingApiKey).build();
-    final GeocodingResult[] results;
-    results =
-        GeocodingApi.geocode(geoCodingApiContext, "")
-            .components(new ComponentFilter("postal_code", postalCode))
-            .await();
-    geoCodingApiContext.shutdown();
-    return results.length == 0 ? Optional.empty() : Optional.of(results[0].geometry.location);
-  }
+            throws Exception {
+        final GeoApiContext geoCodingApiContext =
+                new GeoApiContext.Builder().apiKey(googleGeoCodingApiKey).build();
+        final GeocodingResult[] results;
+        results =
+                GeocodingApi.geocode(geoCodingApiContext, "")
+                        .components(new ComponentFilter("postal_code", postalCode))
+                        .await();
+        geoCodingApiContext.shutdown();
+        return results.length == 0 ? Optional.empty() : Optional.of(results[0].geometry.location);
+    }
 
     private String findTimezoneByLocation(final LatLng latLng)
-      throws Exception {
-    final GeoApiContext mapApiContext = new GeoApiContext.Builder().apiKey(googleGeoCodingApiKey).build();
-    final TimeZone tz = TimeZoneApi.getTimeZone(mapApiContext, latLng).await();
-    mapApiContext.shutdown();
-    return tz.getID();
-  }
+            throws Exception {
+        final GeoApiContext mapApiContext = new GeoApiContext.Builder().apiKey(googleGeoCodingApiKey).build();
+        final TimeZone tz = TimeZoneApi.getTimeZone(mapApiContext, latLng).await();
+        mapApiContext.shutdown();
+        return tz.getID();
+    }
 
 
 }
